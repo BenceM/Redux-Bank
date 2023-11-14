976ca6af49967b2569d88d1d8b52744c6892c875
Bence <m.bence211@gmail.com>
2023-11-14 20:09:49 +0100

    initial commit with working redux store

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import store from "./store.jsx";
+
+store.dispatch({ type: "account/deposit", payload: 500 });
+console.log(store.getState());
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
